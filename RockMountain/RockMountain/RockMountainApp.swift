4bc2ef27275b8fd125347fe0b837d61c068ec634
Gabriel-M-Martins <gabrielmmartinscontato@gmail.com>
2023-04-10 17:22:17 -0300

    new proj

---
+++ RockMountain/RockMountain/RockMountainApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockMountainApp.swift
+//  RockMountain
+//
+//  Created by Gabriel Medeiros Martins on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RockMountainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
